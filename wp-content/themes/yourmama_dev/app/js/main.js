b78919134a34b0ccc956edf39bb6b6f379668594
dantewebmaster <dantewebmaster@outlook.com>
2017-09-27 13:54:42 -0300

    Area restrita(Tratamentos) criada

--- wp-content/themes/yourmama_dev/app/js/main.js
+++ wp-content/themes/yourmama_dev/app/js/main.js
@@ -41,126 +41,140 @@ $( document ).ready(function() {
         icon.addClass('toggle-button-black');
     }
 
-// Ativa o menu
-$(".toggle-button").click(function() {
-    $(".menu-active").slideToggle();
-    var logo = $("#site-logo");
-    var swap = logo.attr("data-swap");
-    var current = logo.attr("src");
-    var data_img = $(".data-img");
+    // Ativa o menu
+    $(".toggle-button").click(function() {
+        $(".menu-active").slideToggle();
+        var logo = $("#site-logo");
+        var swap = logo.attr("data-swap");
+        var current = logo.attr("src");
+        var data_img = $(".data-img");
 
-    $(".menu").toggleClass('fixed');
+        $(".menu").toggleClass('fixed');
+            // Troca a imagem
+            logo.attr('src', swap).attr("data-swap", current);
+            // Troca a cor do icone se for a home do site
+            if (!menu.hasClass('not_home')) {
+                icon.toggleClass('toggle-button-black');
+            }
+            // Troca o icone
+            if(icon.hasClass('fa-bars')){
+                icon.removeClass('fa-bars').addClass('fa-close');
+            } else {
+                icon.addClass('fa-bars').removeClass('fa-close');
+            }
+            // Adiciona Efeitos
+            data_img.toggleClass('wow fadeInUp animated').attr("data-wow-duration", "3s");
+        });
+
+
+    // Hover de elementos do menu
+    $menuItens = $('.menu-items-list .item a');
+    $menuBackground = $('.menu-item-background');
+    $menuItens.each(function(index, el) {
+        $(this).on('mouseover', function(event) {
+            event.preventDefault();
+            $menuBackground.removeClass('active');
+            $($(this).data('item')).addClass('active');
+        });
+    });
+
+    var $owlDiretores = $('.owl-diretores'),
+        $owlWow = $(".wow-home");
+    $owlDiretores.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        smartSpeed: 1300
+    });
+    // Listen to owl events:
+    $owlDiretores.on('translate.owl.carousel', function(event) {
+        $owlWow.addClass('wow fadeInUp animated');
+        $owlWow.on("webkitAnimationEnd oanimationend msAnimationEnd animationend",
+            function() {
+                $owlWow.removeClass('wow fadeInUp animated');
+            }
+        );
+    });
+
+    // Inserir efeitos na página de diretores
+
+    $('.wow-diretores').addClass('wow fadeInUp');
+    $('.wow-cinema-tv').addClass('wow fadeInDown');
+    $('.wow-diretores').css('color', '#fff');
+
+    // Hover da página interna de diretores
+
+    // Hover de elementos da página diretores
+    var $diretorTitle = $(".diretor-title");
+    $diretorTitle.on('mouseover',function(){
+        // Variaveis
+        var img = $(this).attr("data-img");
+        var con = $("#page-diretores");
         // Troca a imagem
-        logo.attr('src', swap).attr("data-swap", current);
-        // Troca a cor do icone se for a home do site
-        if (!menu.hasClass('not_home')) {
-            icon.toggleClass('toggle-button-black');
-        }
-        // Troca o icone
-        if(icon.hasClass('fa-bars')){
-            icon.removeClass('fa-bars').addClass('fa-close');
-        } else {
-            icon.addClass('fa-bars').removeClass('fa-close');
-        }
-        // Adiciona Efeitos
-        data_img.toggleClass('wow fadeInUp animated').attr("data-wow-duration", "3s");
+
+        con.fadeIn('slow', function(){
+            con.css({
+                'background': 'url('+ img +') no-repeat center center',
+                'background-size': 'cover',
+                'transition': '.7s'
+            });
+        });
+
+        $diretorTitle.not(this).css('color', 'transparent');
+    });
+
+    $diretorTitle.on('mouseleave', function(event) {
+        $diretorTitle.css('color', '#fff').fadeIn('slow', function() {});;
     });
 
 
-// Hover de elementos do menu
-$menuItens = $('.menu-items-list .item a');
-$menuBackground = $('.menu-item-background');
-$menuItens.each(function(index, el) {
-    $(this).on('mouseover', function(event) {
-        event.preventDefault();
-        $menuBackground.removeClass('active');
-        $($(this).data('item')).addClass('active');
-    });
-});
+    // Animação da página de Video
+    $(".projeto-open").on('click', function(event) {
+        var titulo = $(this).find('.title').text();
+        var prologo = $(this).find('.read-more').text();
+        var id = $(this).attr("id");
+        var src = $(this).find('.video-src').attr('data-src');
+        var iframe = $('.page-iframe');
+        $("#subtitle").hide();
+        $('#diretor-text .prologo').hide();
+        iframe.attr('src', src);
+        
+        $('html, body').animate({
+            scrollTop: '0px'
+        }, function(){
+            $('#diretor-text .description-all').slideUp(500); // Bio
+            $('.pos').fadeOut(500, function(){ // Diretor(a)
+                $('#subtitle').text(titulo).fadeIn(500); // Nome do video
+                $('#diretor-text .prologo').text(prologo).fadeIn(500); // Nome do video
+            });
 
-var $owlDiretores = $('.owl-diretores'),
-    $owlWow = $(".wow-home");
-$owlDiretores.owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    smartSpeed: 1300
-});
-// Listen to owl events:
-$owlDiretores.on('translate.owl.carousel', function(event) {
-    $owlWow.addClass('wow fadeInUp animated');
-    $owlWow.on("webkitAnimationEnd oanimationend msAnimationEnd animationend",
-        function() {
-            $owlWow.removeClass('wow fadeInUp animated');
-        }
-    );
-});
-
-// Inserir efeitos na página de diretores
-
-$('.wow-diretores').addClass('wow fadeInUp');
-$('.wow-cinema-tv').addClass('wow fadeInDown');
-$('.wow-diretores').css('color', '#fff');
-
-// Hover da página interna de diretores
-
-// Hover de elementos da página diretores
-var $diretorTitle = $(".diretor-title");
-$diretorTitle.on('mouseover',function(){
-    // Variaveis
-    var img = $(this).attr("data-img");
-    var con = $("#page-diretores");
-    // Troca a imagem
-
-    con.fadeIn('slow', function(){
-         con.css({
-            'background': 'url('+ img +') no-repeat center center',
-            'background-size': 'cover',
-            'transition': '.7s'
+            $(".iframe-holder").slideDown(500, function(){
+                $(".project-section").fadeOut(500);
+                console.log(".video-"+id);
+            });
         });
     });
 
-    $diretorTitle.not(this).css('color', 'transparent');
-});
-
-$diretorTitle.on('mouseleave', function(event) {
-    $diretorTitle.css('color', '#fff').fadeIn('slow', function() {});;
-});
 
 
-// Animação da página de Video
-
-
-$(".projeto-open").on('click', function(event) {
-    var titulo = $(this).find('.title').text();
-    var prologo = $(this).find('.read-more').text();
-    var id = $(this).attr("id");
-    var src = $(this).find('.video-src').attr('data-src');
-    var iframe = $('.page-iframe');
-    $("#subtitle").hide();
-    $('#diretor-text .prologo').hide();
-    iframe.attr('src', src);
+    // Finds all iframes from youtubes and gives them a unique class
+    $('iframe[src*="https://www.youtube.com/embed/"]').addClass("youtube-iframe");
     
-    $('html, body').animate({
-        scrollTop: '0px'
-    }, function(){
-        $('#diretor-text .description-all').slideUp(500); // Bio
-        $('.pos').fadeOut(500, function(){ // Diretor(a)
-            $('#subtitle').text(titulo).fadeIn(500); // Nome do video
-            $('#diretor-text .prologo').text(prologo).fadeIn(500); // Nome do video
-        });
-
-        $(".iframe-holder").slideDown(500, function(){
-            $(".project-section").fadeOut(500);
-            console.log(".video-"+id);
-        });
+        $(".modal-close").click(function() {
+            // changes the iframe src to prevent playback or stop the video playback in our case
+            $('.youtube-iframe').each(function(index) {
+            $(this).attr('src', $(this).attr('src'));
+            return false;
+            });
+            
     });
+    
+
 });
-});
+    
 
 // Mostra conteudo novamente
-
 $("#diretor-title").on('click', function(event) {
     $("#subtitle").text("").hide('slow');
     $("#diretor-text .prologo").text("").hide('slow');
--- wp-content/themes/yourmama_dev/functions.php
+++ wp-content/themes/yourmama_dev/functions.php
@@ -152,3 +152,15 @@ if( function_exists('acf_add_options_page') ) {
     'parent_slug' => 'theme-general-settings',
   ));
 }
+
+function my_password_form() {
+    global $post;
+
+    $label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+    $o = '<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" method="post">
+      <label for="' . $label . '">' . __( "Digite <br />sua senha" ) . '</label><input name="post_password" id="' . $label . '" type="password" size="20" maxlength="20" style="width: 160px;" /><input type="submit" name="Submit" value="' . esc_attr__( "Submit" ) . '" />
+    </form>
+    ';
+    return $o;
+}
+add_filter( 'the_password_form', 'my_password_form' );
